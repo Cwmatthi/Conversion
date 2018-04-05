03e2c122fe5a7ab945ca7785f8813ec65d0360c1
Cwmatthi <Codymatthias@gmail.com>
2018-04-05 17:33:20 -0400

    Added NumberConversion and req specs and tech specs

---
+++ NumberConversion/NumberConversion/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace NumberConversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.romanLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.arabicLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBox1.Location = new System.Drawing.Point(184, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 33);
+            this.textBox1.TabIndex = 1;
+            // 
+            // romanLabel
+            // 
+            this.romanLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.romanLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.romanLabel.Location = new System.Drawing.Point(0, 98);
+            this.romanLabel.Name = "romanLabel";
+            this.romanLabel.Size = new System.Drawing.Size(121, 33);
+            this.romanLabel.TabIndex = 2;
+            this.romanLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBox2.Location = new System.Drawing.Point(0, 40);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 33);
+            this.textBox2.TabIndex = 3;
+            // 
+            // arabicLabel
+            // 
+            this.arabicLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.arabicLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.arabicLabel.Location = new System.Drawing.Point(184, 98);
+            this.arabicLabel.Name = "arabicLabel";
+            this.arabicLabel.Size = new System.Drawing.Size(121, 33);
+            this.arabicLabel.TabIndex = 4;
+            this.arabicLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-3, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Arabic :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Roman:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(305, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "To Roman:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "To Arabic:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 179);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.arabicLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.romanLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label romanLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label arabicLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NumberConversion/NumberConversion/RomanToArabic.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConversion
+{
+    class RomanToArabic
+    {
+      public static  Dictionary<char, int> CharValues = null;
+    
+        public static int  RomtoAra( string roman )
+        {
+            if (CharValues == null)
+            {
+                CharValues = new Dictionary<char, int>();
+                CharValues.Add('I', 1);
+                CharValues.Add('V', 5);
+                CharValues.Add('X', 10);
+                CharValues.Add('L', 50);
+                CharValues.Add('C', 100);
+                CharValues.Add('D', 500);
+                CharValues.Add('M', 1000);
+            }
+
+            if (roman.Length == 0) return 0;
+            roman = roman.ToUpper();
+
+            // See if the number begins with (.
+            if (roman[0] == '(')
+            {
+                // Find the closing parenthesis.
+                int pos = roman.LastIndexOf(')');
+
+                // Get the value inside the parentheses.
+                string part1 = roman.Substring(1, pos - 1);
+                string part2 = roman.Substring(pos + 1);
+                return 1000 * RomtoAra(part1) + RomtoAra(part2);
+            }
+
+        // The number doesn't begin with (.
+        // Convert the letters' values.
+        int total = 0;
+            int last_value = 0;
+            for (int i = roman.Length - 1; i >= 0; i--)
+            {
+                int new_value = CharValues[roman[i]];
+
+                // See if we should add or subtract.
+                if (new_value < last_value)
+                    total -= new_value;
+                else
+                {
+                    total += new_value;
+                    last_value = new_value;
+                }
+            }
+
+            // Return the result.
+            return total;
+        }
+    }
+    class ArabicToRoman
+    {
+        public static string[] ThouLetters = { "", "M", "MM", "MMM" };
+        public static string[] HundLetters =
+            { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        public static string[] TensLetters =
+            { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        public static string[] OnesLetters =
+            { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+        public  static string AratoRom(int arabic)
+        {
+            //See if it's >= 4000.
+            try
+            {
+
+           
+            if (arabic >= 4000)
+            {
+                //Use parentheses.
+                //int thou = arabic / 1000;
+                //arabic %= 1000;
+                //return "(" + AratoRom(thou) + ")" +
+                //    AratoRom(arabic);
+             throw new Exception("Number cannot be over 3999");
+            }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            // Otherwise process the letters.
+            string result = "";
+            
+            // Pull out thousands.
+            int num;
+            num = arabic / 1000;
+            result += ThouLetters[num];
+            arabic %= 1000;
+
+            // Handle hundreds.
+            num = arabic / 100;
+            result += HundLetters[num];
+            arabic %= 100;
+
+            // Handle tens.
+            num = arabic / 10;
+            result += TensLetters[num];
+            arabic %= 10;
+
+            // Handle ones.
+            result += OnesLetters[arabic];
+
+            return result;
+            
+        }
+    }
+    
+}
